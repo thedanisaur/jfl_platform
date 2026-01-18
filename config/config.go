e50ae79a7a13ce49e4091a149d175027e9ecbdd0
Daniel DeFinis <definisdan@yahoo.com>
2026-01-18 00:12:23 -0500

    Allow LoadConfig function to be called

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ import (
 	"github.com/thedanisaur/jfl_platform/types"
 )
 
-func loadConfig(config_path string) (types.Config, error) {
+func LoadConfig(config_path string) (types.Config, error) {
 	var config types.Config
 	config_file, err := os.Open(config_path)
 	if err != nil {
